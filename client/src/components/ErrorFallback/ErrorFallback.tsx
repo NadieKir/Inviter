0df640f76c9a7a9a4b770c4312a6290aaa46e9ef
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2023-03-25 22:19:40 +0300

    Add ForbiddenPage

--- client/src/components/ErrorFallback/ErrorFallback.tsx
+++ client/src/components/ErrorFallback/ErrorFallback.tsx
@@ -11,11 +11,11 @@ export const ErrorFallback = ({
   error,
   resetErrorBoundary,
 }: ErrorFallbackProps) => (
-  <div className={styles.wrapper} role="alert">
+  <section className={styles.wrapper} role="alert">
     <h1 className={styles.heading}>Что-то пошло не так</h1>
     <p className={styles.message}>{error.message}</p>
     <Button size={ButtonSize.Small} onClick={resetErrorBoundary}>
       Попробовать снова
     </Button>
-  </div>
+  </section>
 );
--- client/src/pages/ForbiddenPage/ForbiddenPage.tsx
+++ client/src/pages/ForbiddenPage/ForbiddenPage.tsx
@@ -1,14 +1,11 @@
-import styles from "./ForbiddenPage.module.scss";
+import styles from './ForbiddenPage.module.scss';
 
-export const ForbiddenPage = () => {
-  return (
-    // <div className={styles.wrapper}>
-    //   <div className={styles.numbers}>403</div>
-    //   <div className={styles.description}>
-    //     Вы не имеете прав для доступа к этой странице
-    //     <span className={styles.blink}>_</span>
-    //   </div>
-    // </div>
-    <div>f</div>
-  );
-};
+export const ForbiddenPage = () => (
+  <section className={styles.wrapper}>
+    <span className={styles.numbers}>403</span>
+    <h1 className={styles.heading}>
+      Вы не имеете прав для доступа к этой странице
+      <span className={styles.blink}>_</span>
+    </h1>
+  </section>
+);
