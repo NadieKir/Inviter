8df446a6369d60939974a50e7121c99a85ec32ff
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2023-04-09 12:34:29 +0300

    Some fix

--- client/src/pages/UserPage/UserPage.tsx
+++ client/src/pages/UserPage/UserPage.tsx
@@ -2,6 +2,7 @@ import { Button } from 'components';
 
 import styles from './UserPage.module.scss';
 import mockUser from 'assets/images/mock-user-photo.jpg';
+import classNames from 'classnames';
 
 export const UserPage = () => {
   return (
@@ -14,23 +15,74 @@ export const UserPage = () => {
             className={styles.userPhoto}
           />
           <Button>Подписаться</Button>
-          <h2>Анкета</h2>
-          <div className={styles.questionnaire}>
-            <div className={styles.questionnaireRow}></div>
-            <div className={styles.questionnaireRow}></div>
-            <div className={styles.questionnaireRow}></div>
-            <div className={styles.questionnaireRow}></div>
-            <div className={styles.questionnaireRow}></div>
+          <h2
+            className={classNames(
+              styles.headingH2,
+              styles.questionnaireHeading,
+            )}
+          >
+            Анкета
+          </h2>
+
+          <div className={styles.questionnairesWrapper}>
+            <div className={styles.questionnaire}>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+            </div>
+            <div className={styles.questionnaire}>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+              <div className={styles.questionnaireRow}>
+                <span className={styles.subject}>День рождения</span>
+                <span className={styles.description}>21.08.2000</span>
+              </div>
+            </div>
           </div>
         </div>
         <div className={styles.briefInterestsWrapper}>
           <div className={styles.brief}>
-            <h1>Ангелина, 22</h1>
+            <h1 className={styles.headingH1}>Ангелина, 22</h1>
             <div className={styles.nicknameCityWrapper}></div>
             <p className={styles.description}></p>
           </div>
           <div className={styles.interests}>
-            <h2>Интересы</h2>
+            <h2 className={styles.headingH2}>Интересы</h2>
             <div className={styles.interestsTags}></div>
             <p className={styles.matchRate}>
               Совпадение интересов: <span className="blue">33%</span>
