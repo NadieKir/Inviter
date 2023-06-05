7c1cfbb7427df7eea79ea5ee006f075323513dde
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2023-06-05 23:17:12 +0300

    Add suitable button to main page

--- client/src/pages/SearchInvitePage/SearchInvitePage.tsx
+++ client/src/pages/SearchInvitePage/SearchInvitePage.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { observer, useLocalObservable } from 'mobx-react-lite';
 
 import { InviteCard, Loader, NothingFound } from 'components';
@@ -6,11 +6,13 @@ import { SearchInviteForm } from 'forms';
 import { AnotherUsersInvitesStore } from 'stores';
 import { UserContext } from 'common/contexts';
 import { createOption } from 'types';
+import { getAge, isAgeSuitable } from 'common/helpers';
 
 import styles from './SearchInvitePage.module.scss';
 
 export const SearchInvitePage = observer(() => {
   const { user, loadResponses } = useContext(UserContext);
+  const [suitableOnly, setSuitableOnly] = useState(false);
 
   if (!user) {
     return null;
@@ -19,10 +21,51 @@ export const SearchInvitePage = observer(() => {
   const { isLoading, anotherUsersInvites, getAnotherUsersInvites } =
     useLocalObservable(() => new AnotherUsersInvitesStore({ city: user.city }));
 
+  const renderInvites = () => {
+    let invitesToShow = anotherUsersInvites;
+
+    if (suitableOnly) {
+      invitesToShow = invitesToShow.filter(
+        (i) =>
+          i.companionGender.includes(user.gender) &&
+          (i.companionAge
+            ? isAgeSuitable(getAge(new Date(user.birthday)), i.companionAge)
+            : true),
+      );
+    }
+
+    if (invitesToShow.length === 0) return <NothingFound />;
+
+    return invitesToShow.map((invite) => (
+      <InviteCard
+        key={invite._id}
+        invite={invite}
+        onInviteAction={loadResponses}
+      />
+    ));
+  };
+
   return (
     <section className={styles.section}>
       <div className={styles.searchForm}>
-        <h1 className="heading-H1">Чем хотите заняться?</h1>
+        <div className={styles.headingBtnWrapper}>
+          <h1 className="heading-H1">Чем хотите заняться?</h1>
+          {suitableOnly ? (
+            <button
+              className={styles.btn}
+              onClick={() => setSuitableOnly(false)}
+            >
+              Показать все
+            </button>
+          ) : (
+            <button
+              className={styles.btn}
+              onClick={() => setSuitableOnly(true)}
+            >
+              Показать только подходящие
+            </button>
+          )}
+        </div>
         <SearchInviteForm
           initialFilters={{
             city: createOption(user.city),
@@ -33,15 +76,7 @@ export const SearchInvitePage = observer(() => {
       {isLoading ? (
         <Loader />
       ) : (
-        <ul className={styles.userCards}>
-          {anotherUsersInvites.length === 0 ? (
-            <NothingFound />
-          ) : (
-            anotherUsersInvites.map((invite) => (
-              <InviteCard key={invite._id} invite={invite} onInviteAction={loadResponses} />
-            ))
-          )}
-        </ul>
+        <ul className={styles.userCards}>{renderInvites()}</ul>
       )}
     </section>
   );
