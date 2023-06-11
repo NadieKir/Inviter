f7884730e91553d6f61b149aeb210dadfba00913
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2023-06-12 00:26:50 +0300

    Minor fixes

--- client/src/components/NavTabs/NavTabs.tsx
+++ client/src/components/NavTabs/NavTabs.tsx
@@ -7,24 +7,24 @@ import { TabsIndicator, Scrollable } from 'components';
 import styles from './NavTabs.module.scss';
 
 interface NavTabsProps extends HTMLAttributes<HTMLElement> {
-    children: Array<ReactElement<typeof NavLink>>;
+  children: Array<ReactElement<typeof NavLink>>;
 }
 
 export const NavTabs = ({ className, children }: NavTabsProps) => {
-    const location = useLocation();
-    const arrayChildren = Children.toArray(children) as ReactElement[];
-    const tabToOpen = location.pathname.split('/').slice(-1)[0];
-    const indexOfTabToOpen = arrayChildren
-        .map((child) => child.props.to)
-        .indexOf(tabToOpen);
+  const location = useLocation();
+  const arrayChildren = Children.toArray(children) as ReactElement[];
+  const tabToOpen = location.pathname.split('/').slice(-1)[0];
+  const indexOfTabToOpen = arrayChildren
+    .map((child) => child.props.to)
+    .indexOf(tabToOpen);
 
-    return (
-        <Scrollable>
-            <div className={classNames(styles.tabs, className)}>{children}</div>
-            <TabsIndicator
-                tabsAmount={arrayChildren.length}
-                currentTab={indexOfTabToOpen < 0 ? 0 : indexOfTabToOpen}
-            />
-        </Scrollable>
-    );
+  return (
+    <Scrollable>
+      <div className={classNames(styles.tabs, className)}>{children}</div>
+      <TabsIndicator
+        tabsAmount={arrayChildren.length}
+        currentTab={indexOfTabToOpen < 0 ? 0 : indexOfTabToOpen}
+      />
+    </Scrollable>
+  );
 };
--- client/src/forms/RegistrationForm/components/LoginField.tsx
+++ client/src/forms/RegistrationForm/components/LoginField.tsx
@@ -1,73 +1,79 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { checkLogin } from "api";
+import { checkLogin } from 'api';
 
-import { Input, InputField, InputFieldExternalProps, InputRenderProps, InputType } from "components";
-import { FormikValues, useFormikContext } from "formik";
-
+import {
+  Input,
+  InputField,
+  InputFieldExternalProps,
+  InputRenderProps,
+  InputType,
+} from 'components';
+import { FormikValues, useFormikContext } from 'formik';
 
 type Props = {
-    placeholderText?: string;
-    pattern?: string;
-    className?: string;
-    onBlur?: (value: string) => void;
-    onChange?: (value: string) => void;
-}
+  placeholderText?: string;
+  pattern?: string;
+  className?: string;
+  onBlur?: (value: string) => void;
+  onChange?: (value: string) => void;
+};
 
 type LoginFieldProps = InputFieldExternalProps & Props;
 
 export const LoginField = ({
-    name,
-    className,
-    placeholderText,
-    onBlur,
-    onChange,
-    ...rest
+  name,
+  className,
+  placeholderText,
+  onBlur,
+  onChange,
+  ...rest
 }: LoginFieldProps) => {
-    const { errors, setFieldError } = useFormikContext<FormikValues>();
-    const [loginToValidate, setLoginToValidate] = useState('');
-    const error = errors[name] as string;
+  const { errors, setFieldError } = useFormikContext<FormikValues>();
+  const [loginToValidate, setLoginToValidate] = useState('');
+  const error = errors[name] as string;
 
-    useEffect(() => {
-        (async () => {
-            if (!loginToValidate) {
-                return true;
-            }
+  useEffect(() => {
+    (async () => {
+      if (!loginToValidate) {
+        return true;
+      }
 
-            var isLoginExist = await checkLogin(loginToValidate);
-            if (isLoginExist) {
-                setFieldError(name, 'Введеный логин уже существует');
-            } else {
-                setFieldError(name, undefined);
-            }
-        })()
-    }, [name, loginToValidate, setFieldError]);
+      let isLoginExist = await checkLogin(loginToValidate);
+      if (isLoginExist) {
+        setFieldError(name, 'Введеный логин уже существует');
+      } else {
+        setFieldError(name, undefined);
+      }
+    })();
+  }, [name, loginToValidate, setFieldError]);
 
-    return (
-        <InputField
-            name={name}
-            containerAttributes={{ className: className }}
-            forcedError={error}
-            {...rest}>
-            {({ field, className }: InputRenderProps): JSX.Element => {
-                return (
-                    <Input
-                        {...field}
-                        onBlur={(e) => {
-                            field.onBlur(e);
-                            onBlur?.(e.target.value);
-                            setLoginToValidate(e.target.value);
-                        }}
-                        onChange={(e) => {
-                            field.onChange(e);
-                            onChange?.(e.currentTarget.value);
-                        }}
-                        className={className}
-                        placeholder={placeholderText}
-                        type={InputType.Text}
-                    />
-                );
+  return (
+    <InputField
+      name={name}
+      containerAttributes={{ className: className }}
+      forcedError={error}
+      {...rest}
+    >
+      {({ field, className }: InputRenderProps): JSX.Element => {
+        return (
+          <Input
+            {...field}
+            onBlur={(e) => {
+              field.onBlur(e);
+              onBlur?.(e.target.value);
+              setLoginToValidate(e.target.value);
             }}
-        </InputField>
-    );
-}
+            onChange={(e) => {
+              field.onChange(e);
+              onChange?.(e.currentTarget.value);
+            }}
+            className={className}
+            placeholder={placeholderText}
+            type={InputType.Text}
+          />
+        );
+      }}
+    </InputField>
+  );
+};
